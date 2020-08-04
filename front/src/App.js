2d089d292386cfe5448ae8a9b148d930500d6bb1
Olav Fosse <fosseolav@gmail.com>
2020-08-04 05:15:33 +0200

    Save key in cookies

--- front/src/App.js
+++ front/src/App.js
@@ -7,12 +7,14 @@ import threadService from './services/threads'
 const App = () => {
   const [newKey, setNewKey] = useState('')
   const [threads, setThreads] = useState(null)
-  const [key, setKey] = useState(null)
+  const [key, setKey] = useState(localStorage.getItem('key'))
 
   useEffect(() => {
     if(key === null){
       return
     }
+    localStorage.setItem('key', key)
+
     const fetchThreads = async () => {
       const threads = await threadService.getAll(key)
       setThreads(threads)
