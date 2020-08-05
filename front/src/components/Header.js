9a7df023f848a6b1da28a1dc6a9209e94fdc6717
Olav Fosse <fosseolav@gmail.com>
2020-08-06 00:35:40 +0200

    Add dummy thread form

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ThreadForm from './ThreadForm'
 import banner from './banner.png'
 
-const Header = () => (
-  <div id="header">
-    <img src={banner} alt="banner" />
-    <div id="board-title">board.fossegr.im</div>
-    <div id="thread-starter">
-      [<button>Start a New Thread</button>]
+const Header = () => {
+  const [showThreadForm, setShowThreadForm] = useState(false)
+
+  return (
+    <div id="header">
+      <img src={banner} alt="banner" />
+      <div id="board-title">board.fossegr.im</div>
+      {
+        showThreadForm
+          ? <ThreadForm />
+          : (
+            <div id="thread-starter">
+              [<button onClick={_ => setShowThreadForm(true)}> Start a New Thread</button>]
+            </div>
+          )
+      }
     </div>
-  </div>
-)
+  )
+}
 
 export default Header
---
+++ front/src/components/ThreadForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ThreadForm = () => {
+  return (
+    <form id="thread-form">
+      <textarea type="text  "/>
+      <br/>
+      <input type="submit" value="Post"/>
+    </form>
+  )
+}
+
+export default ThreadForm
